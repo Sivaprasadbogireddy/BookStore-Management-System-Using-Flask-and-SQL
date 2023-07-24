5a68befae22873c5465c994b2d50fc2a91b185ad
Sivaprasadramb <sivaprasadreddyb2000@gmail.com>
2023-07-24 14:57:09 -0500

    Create Tables in database
    Create Tables in Database bookstore.db and adding root to the home page (index.html)

--- main.py
+++ main.py
@@ -13,8 +13,55 @@ def get_db():
         db = g._database = sqlite3.connect('bookstore.db')
     return db
 
+# Create the database tables
+with app.app_context():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Publisher (
+                        publisher_id TEXT PRIMARY KEY,
+                        publisher_name TEXT,
+                        publication_address TEXT,
+                        publication_phone TEXT,
+                        publication_web TEXT
+                    )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Books (
+                        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT,
+                        author TEXT,
+                        price REAL,
+                        publication_year INTEGER
+                    )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Publisher_Books_Junction (
+                        publisher_id TEXT,
+                        book_id INTEGER,
+                        FOREIGN KEY (publisher_id) REFERENCES Publisher (publisher_id),
+                        FOREIGN KEY (book_id) REFERENCES Books (book_id),
+                        PRIMARY KEY (publisher_id, book_id)
+                    )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Customers (
+                        customer_id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        email TEXT,
+                        phone TEXT
+                    )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Orders (
+                        order_id INTEGER PRIMARY KEY,
+                        customer_id INTEGER,
+                        book_id INTEGER,
+                        quantity INTEGER,
+                        order_date TEXT,
+                        Total_price INTEGER,
+                        FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+                        FOREIGN KEY (book_id) REFERENCES Books (book_id)
+                    )''')
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-
+# Home page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 
