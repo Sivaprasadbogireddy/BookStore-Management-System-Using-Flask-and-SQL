7d29029c5a6d3ad187e6cc1a82b9c569d6eb9b81
Sivaprasadramb <sivaprasadreddyb2000@gmail.com>
2023-07-24 13:55:28 -0500

    Created main.py
    Created main.py to create bookstore.db

---
+++ main.py
@@ -0,0 +1,22 @@
+# main.py
+from flask import Flask, render_template, request, redirect, g
+import sqlite3
+import uuid
+from flask import render_template
+
+app = Flask(__name__)
+
+# Database connection helper function
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('bookstore.db')
+    return db
+
+
+
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
+
